cabbb1482b0f26121ca553bb3f1b8ae7b78736ba
Shaheryar Ahmed <shaheryar.ahmed.ten@gmail.com>
2024-07-25 14:48:51 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}>Home</Route>
+        <Route path='/about' element={<About />}>About</Route>
+      </Routes>
+      </BrowserRouter>
+
+  )
+}
+
+export default App
